74db910f703aa39f041b43bcb743e74395d138b1
Ace <ahamblin7@gmail.com>
2024-04-23 01:29:46 +0100

    Initial Commit

---
+++ Questy/QuestyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestyApp.swift
+//  Questy
+//
+//  Created by Hamblin, Alastair on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuestyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
